333b196d8457507e624dd020f8f9d392e28fd6c5
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-03-16 21:45:54 -0600

    Clase 16/03/18

--- app/src/main/java/org/unitec/mensajitoandroid/MainActivity.java
+++ app/src/main/java/org/unitec/mensajitoandroid/MainActivity.java
@@ -3,6 +3,7 @@ package org.unitec.mensajitoandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//AppCompatActivity es la que permite la compatibilidad de versiones de android con la aplicación
 public class MainActivity extends AppCompatActivity {
 
     @Override
