5b62d8a568108b90fca54eab76a4f32f9cb5ba36
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-03-20 21:43:34 -0600

    Clase 20/03/18

--- app/src/main/java/org/unitec/mensajitoandroid/MainActivity.java
+++ app/src/main/java/org/unitec/mensajitoandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package org.unitec.mensajitoandroid;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //Funcion asincronica
+    /*
+    Funciones para llamadas asincronicas
+    1.-Preexecute
+    2.-Postexecute
+    3.-Background
+    * */
+    public class TareaBuscarMensajes extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
     }
 }
